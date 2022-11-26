a2e97c9e48a94ce6a1aad8383e752f4e59b80554
Daniil Yakimov <8spider-man@mail.ru>
2022-11-26 13:01:47 +0300

    test 3 methods

--- src/main/kotlin/com/itmo/microservices/order/api/OrderEvents.kt
+++ src/main/kotlin/com/itmo/microservices/order/api/OrderEvents.kt
@@ -33,14 +33,14 @@ data class ItemAddedToOrderEvent(
     name = POSITION_ADDED_TO_ORDER
 )
 
-@DomainEvent(name = POSITION_REMOVED_FROM_ORDER)
-data class ItemRemovedFromOrderEvent(
-    val itemId: UUID,
-    val itemCount: UUID,
-    val orderId: UUID,
-) : Event<OrderAggregate>(
-    name = POSITION_REMOVED_FROM_ORDER
-)
+//@DomainEvent(name = POSITION_REMOVED_FROM_ORDER)
+//data class ItemRemovedFromOrderEvent(
+//    val itemId: UUID,
+//    val itemCount: UUID,
+//    val orderId: UUID,
+//) : Event<OrderAggregate>(
+//    name = POSITION_REMOVED_FROM_ORDER
+//)
 
 @DomainEvent(name = ORDER_BOOKED)
 data class OrderBookedEvent(
@@ -49,52 +49,52 @@ data class OrderBookedEvent(
     name = ORDER_BOOKED
 )
 
-@DomainEvent(name = ORDER_BOOKING_CANCELED)
-data class OrderBookingCanceledEvent(
-    val orderId: UUID,
-) : Event<OrderAggregate>(
-    name = ORDER_BOOKING_CANCELED
-)
-
-@DomainEvent(name = ORDER_EXPIRED)
-data class OrderExpiredEvent(
-    val orderId: UUID,
-) : Event<OrderAggregate>(
-    name = ORDER_EXPIRED
-)
-
-@DomainEvent(name = ORDER_PAID)
-data class OrderPaidEvent(
-    val orderId: UUID,
-) : Event<OrderAggregate>(
-    name = ORDER_PAID
-)
-
-@DomainEvent(name = ORDER_DELIVERY_STARTED)
-data class OrderDeliveryStartedEvent(
-    val orderId: UUID,
-) : Event<OrderAggregate>(
-    name = ORDER_DELIVERY_STARTED
-)
-
-@DomainEvent(name = ORDER_DELIVERY_FAILED)
-data class OrderDeliveryFailedEvent(
-    val orderId: UUID,
-) : Event<OrderAggregate>(
-    name = ORDER_DELIVERY_FAILED
-)
-
-@DomainEvent(name = ORDER_DELIVERY_COMPLETED)
-data class OrderDeliveryCompletedEvent(
-    val orderId: UUID,
-) : Event<OrderAggregate>(
-    name = ORDER_DELIVERY_COMPLETED
-)
-
-@DomainEvent(name = ORDER_DELIVERY_SLOT_UPDATED)
-data class OrderDeliverySlotUpdatedEvent(
-    val orderId: UUID,
-    val slotInSec: Int,
-) : Event<OrderAggregate>(
-    name = ORDER_DELIVERY_SLOT_UPDATED
-)
+//@DomainEvent(name = ORDER_BOOKING_CANCELED)
+//data class OrderBookingCanceledEvent(
+//    val orderId: UUID,
+//) : Event<OrderAggregate>(
+//    name = ORDER_BOOKING_CANCELED
+//)
+//
+//@DomainEvent(name = ORDER_EXPIRED)
+//data class OrderExpiredEvent(
+//    val orderId: UUID,
+//) : Event<OrderAggregate>(
+//    name = ORDER_EXPIRED
+//)
+//
+//@DomainEvent(name = ORDER_PAID)
+//data class OrderPaidEvent(
+//    val orderId: UUID,
+//) : Event<OrderAggregate>(
+//    name = ORDER_PAID
+//)
+//
+//@DomainEvent(name = ORDER_DELIVERY_STARTED)
+//data class OrderDeliveryStartedEvent(
+//    val orderId: UUID,
+//) : Event<OrderAggregate>(
+//    name = ORDER_DELIVERY_STARTED
+//)
+//
+//@DomainEvent(name = ORDER_DELIVERY_FAILED)
+//data class OrderDeliveryFailedEvent(
+//    val orderId: UUID,
+//) : Event<OrderAggregate>(
+//    name = ORDER_DELIVERY_FAILED
+//)
+//
+//@DomainEvent(name = ORDER_DELIVERY_COMPLETED)
+//data class OrderDeliveryCompletedEvent(
+//    val orderId: UUID,
+//) : Event<OrderAggregate>(
+//    name = ORDER_DELIVERY_COMPLETED
+//)
+//
+//@DomainEvent(name = ORDER_DELIVERY_SLOT_UPDATED)
+//data class OrderDeliverySlotUpdatedEvent(
+//    val orderId: UUID,
+//    val slotInSec: Int,
+//) : Event<OrderAggregate>(
+//    name = ORDER_DELIVERY_SLOT_UPDATED
+//)
---
+++ src/test/kotlin/com/itmo/microservices/order/OrderAggregateStateTest.kt
@@ -0,0 +1,82 @@
+package com.itmo.microservices.order
+
+import com.itmo.microservices.order.api.OrderAggregate
+import com.itmo.microservices.order.logic.OrderAggregateState
+import com.itmo.microservices.order.model.OrderStatus
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.query.Criteria
+import org.springframework.data.mongodb.core.query.Query
+import ru.quipy.core.EventSourcingService
+import java.util.*
+
+@SpringBootTest
+class OrderAggregateStateTest {
+
+    companion object {
+        private val testId = UUID.randomUUID()
+        private val userId = UUID.randomUUID()
+        private val itemId = UUID.randomUUID()
+        private val itemAmount = 10
+    }
+
+    @Autowired
+    private lateinit var orderESService: EventSourcingService<UUID, OrderAggregate, OrderAggregateState>
+
+    @Autowired
+    lateinit var mongoTemplate: MongoTemplate
+
+    @BeforeEach
+    fun init() {
+        cleanDatabase()
+    }
+
+    fun cleanDatabase() {
+        mongoTemplate.remove(Query.query(Criteria.where("aggregateId").`is`(testId)), "orders")
+        mongoTemplate.remove(Query.query(Criteria.where("_id").`is`(testId)), "snapshots")
+    }
+
+    @Test
+    fun createOrder() {
+        val event = orderESService.create {
+            it.createOrder(userId.toString())
+        }
+
+        val state = orderESService.getState(event.orderId)!!
+
+        Assertions.assertEquals(event.userId, state.getUserId())
+        Assertions.assertEquals(event.orderId, state.getId())
+    }
+
+    @Test
+    fun addItemToOrder() {
+        val createdOrderEvent = orderESService.create {
+            it.createOrder(userId.toString())
+        }
+
+        val addItemToOrder = orderESService.update(createdOrderEvent.orderId) {
+            it.addItemToOrder(createdOrderEvent.orderId, itemId, itemAmount)
+        }
+        val state = orderESService.getState(addItemToOrder.orderId)!!
+
+        Assertions.assertEquals(itemAmount, state.getOrderItemsAmount().get(addItemToOrder.itemId))
+    }
+
+    @Test
+    fun bookOrder() {
+        val createdOrderEvent = orderESService.create {
+            it.createOrder(userId.toString())
+        }
+
+        val bookOrderEvent = orderESService.update(createdOrderEvent.orderId) {
+            it.bookOrder(createdOrderEvent.orderId)
+        }
+        val state = orderESService.getState(bookOrderEvent.orderId)!!
+
+        Assertions.assertEquals(state.getStatus(), OrderStatus.BOOKED)
+    }
+}
