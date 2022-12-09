ddadf93ddc77c8841224caad8cfefb78f4460aef
ruaglsc <stanislav.ugolnikov@raiffeisen.ru>
2022-12-09 23:41:43 +0300

    added test for contextLoads

---
+++ src/test/kotlin/com/itmo/microservices/order/OrderServiceApplicationTest.kt
@@ -0,0 +1,12 @@
+package com.itmo.microservices.order
+
+import org.junit.jupiter.api.Test
+
+
+class OrderServiceApplicationTests : BaseIntegrationTest() {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
